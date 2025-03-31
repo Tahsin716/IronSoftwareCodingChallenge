1c671581831fc245116b2f5dcd7ea5c31d86f7c7
Tahsin Rashad <tahsin.rashad716@gmail.com>
2025-03-31 15:50:09 +0100

    service registration implemented for DI

---
+++ src/PhoneParser/DI/ServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using Ninject;
+using PhoneParser.Contracts;
+using PhoneParser.Services;
+
+namespace PhoneParser.DI
+{
+    public static class ServiceRegistration
+    {
+        private static readonly StandardKernel _kernel = new StandardKernel();
+
+        static ServiceRegistration()
+        {
+            RegisterServices();
+        }
+
+        private static void RegisterServices()
+        {
+            _kernel.Bind<IOldPhoneParser>().To<OldPhoneParser>().InSingletonScope();
+        }
+
+        public static T GetService<T>()
+        {
+            return _kernel.Get<T>();
+        }
+    }
+}
