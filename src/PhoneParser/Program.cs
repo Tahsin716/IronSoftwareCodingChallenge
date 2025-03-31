24382cedf237ec0fa529b06504052f46118b740a
Tahsin Rashad <tahsin.rashad716@gmail.com>
2025-03-31 16:15:00 +0100

    PhoneParser changed to a console app

---
+++ src/PhoneParser/Program.cs
@@ -0,0 +1,15 @@
+﻿using PhoneParser.Contracts;
+using PhoneParser.DI;
+
+namespace PhoneParser
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var oldPhoneParser = ServiceRegistration.GetService<IOldPhoneParser>();
+            var result = oldPhoneParser.OldPhonePad("4433555 555666#");
+            Console.WriteLine(result);
+        }
+    }
+}
--- test/PhoneParserTest/OldPhoneParserTests.cs
+++ test/PhoneParserTest/OldPhoneParserTests.cs
@@ -1,18 +1,25 @@
-using PhoneParser.Services;
+using PhoneParser.Contracts;
+using PhoneParser.DI;
 
 namespace PhoneParserTest
 {
     public class OldPhoneParserTests
     {
+        private readonly IOldPhoneParser OldPhoneParser;
+        public OldPhoneParserTests()
+        {
+            OldPhoneParser = ServiceRegistration.GetService<IOldPhoneParser>();
+        }
+
         [Theory]
-        [InlineData("33#", "E")]  
-        [InlineData("227*#", "B")]  
+        [InlineData("33#", "E")]
+        [InlineData("227*#", "B")]
         [InlineData("4433555 555666#", "HELLO")]
         [InlineData("8 88777444666*664#", "TURING")]
         public void OldPhonePad_ReturnsExpectedOutput(string input, string expected)
         {
-            //string result = OldPhoneParser.OldPhonePad(input);
-            //Assert.Equal(expected, result);
+            string result = OldPhoneParser.OldPhonePad(input);
+            Assert.Equal(expected, result);
         }
     }
 }
